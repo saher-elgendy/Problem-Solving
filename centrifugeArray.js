d08128225496190988845c7718f5a97282438e7e
unknown <saherelgendy1990@gmail.com>
2018-10-16 01:48:23 +0200

    add solution 1

---
+++ centrifugeArray.js
@@ -0,0 +1,50 @@
+/*
+Write a function that takes an array of numbers as arguments and returns an array of numbers represents the original array but sorted by the sum of every two opposite elements in the array, the least sum
+put the two elements toward the middle of the array , the bigger should be toward the extremities,
+
+Every two opposite elements means that if one is at index i the opposite should be at index (length-1-i) so element at index 0 is the opposite of element at index 6 in 7 elements array.
+
+For example
+
+sortSum([1,5,4,6]); // [5,1,6,4]
+sortSum([2,14,6,5,8,17,3]); // [14,6,2,5,3,8,17]
+Explanation of last example
+
+2 is opposite to 3,
+14=>17,
+6 =>8 ,
+5 is in the middle=> no opposite element, so now 2 and 3 are toward the middle, then 6 and 8 then 14 and 17 at extremities
+*/
+
+//solution 1 
+
+const arrange = (arr) => {
+  let firstHalf = [];
+  let secondHalf = [];
+  let finalResults = [];
+
+  if(arr.length % 2 === 1) {
+	const middle = arr[Math.floor(arr.length/2)];
+    finalResults.push(middle);
+    arr.splice(Math.floor(arr.length/2),1);
+  }
+
+  for(let i = 0 ; i < (arr.length/2) ; i++) {	
+	firstHalf.push(arr[i]);
+	secondHalf.push(arr[arr.length - i-1])
+  }
+
+  firstHalf.sort((a, b) => b - a).reverse();
+
+  secondHalf.sort((a, b) => a - b);
+
+  for(let i =0 ; i< firstHalf.length; i++) {
+    finalResults.unshift(firstHalf[i]);
+	finalResults.push(secondHalf[i]);
+  }
+
+  return finalResults;
+}
+
+console.log(arrange([0,2,7,4,3])); // [2, 0, 7, 3, 4]
+
