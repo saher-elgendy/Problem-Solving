ad921fab287d781f847181d4af1c2e616d1b3ad1
unknown <saherelgendy1990@gmail.com>
2018-10-29 19:01:58 +0200

    introducing solution 1

---
+++ unique string/uniqueString.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+/*
+The unique string
+
+Write a function that can take two or more strings as arguments and returns a string represent them all , all characters should be in same order in the original string but no duplication in the returned string
+
+For example
+
+uniqueStr("abcd","ahkd") // abcdhk
+uniqueStr("rbanr","fzyrc") // rbanfzyc
+uniqueStr("I","am","passionate", 'problem', 'solver') // Iampsionterblv
+
+consider uppercase and lowercase letters are different characters
+
+*/
+
+
+//Solution 1
+
+const uniqueString = (...args) => {
+  let results = [];
+  args.forEach(word => [...word].forEach(letter => !results.includes(letter) ? results.push(letter) : ''))
+  return results.join('');
+}
+
+uniqueString('abcd', 'cdfg')//'abcdfg'
+uniqueString("rbanr","fzyrc"); //rbanfzyc
+--------------------------------------------------------------------------------------------------
+
