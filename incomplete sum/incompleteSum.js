59c9f939eb092c9d391da9f13f6bc47ad12348db
unknown <saherelgendy1990@gmail.com>
2018-10-29 07:15:19 +0200

    add problem explanation

---
+++ incomplete sum/incompleteSum.js
@@ -0,0 +1,21 @@
+
+/*
+Incomplete sum
+Write a function that takes an array of numbers as argument and return the sum of the largest and smallest numbers and all numbers in between except numbers found in the array
+
+For example
+
+getIncompleteSum([2,3,5])   // 11
+getIncompleteSum([3,6,2,1,4]); // 12
+getIncompleteSum([1,3,5,10]); //47
+
+
+more explanation on te last example
+
+the biggest number is 10, the smallest one is 1 , now we need to get the sum of 
+
+10 + 1 + 2 + 4 + 6 + 7 + 8 + 9 = 47
+
+3 and 5 not included because they exist in the array.
+
+*/
