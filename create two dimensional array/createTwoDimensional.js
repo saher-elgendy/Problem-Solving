e91c7df50551da834818af366b0e4e6c61b1fafc
unknown <saherelgendy1990@gmail.com>
2018-10-31 06:18:10 +0200

    adding problem explanation

---
+++ create two dimensional array/createTwoDimensional.js
@@ -0,0 +1,13 @@
+/*
+
+Write a function that takes two arguments , the first one should be an array and the second one
+is a number , the function should return a two dimensional array, each element in the new array 
+an array with number of elements equal to the second argument of the function as following examples
+
+
+createTwoDimensional([1, 2, 5, 13, 8, 9], 2); //  [[1,2], [5,13], [8,9]]
+createTwoDimensional([1, 2, 5, 13, 8, 9, 6], 2); //  [[1,2], [5,13], [8,9], [6]]
+createTwoDimensional([6, 10, 15, 2, 9, 27, 100, 0, 3, 4, 36], 4); // [[6, 10, 15, 2], [9, 27, 100, 0], [3, 4, 36]]
+
+
+*/
