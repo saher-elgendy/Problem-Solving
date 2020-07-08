e9d7921fecd0d4e13fbd9e7b4d6cda665077e81f
unknown <saherelgendy1990@gmail.com>
2020-07-08 00:19:29 -0700

    introducing solution 1 - Recursion

---
+++ group numbers/index.js
@@ -0,0 +1,20 @@
+/*
+  input: [6.1, 4.2, 6.3]
+  function: Math.floor
+  output: { '4': [ 4.2 ], '6': [ 6.1, 6.3 ] }
+*/
+
+// solution:
+const groupBy= (numbers, fn, bag = {}) => {
+    if(!numbers.length) return bag;
+    const firstEl = fn(numbers[0]);
+    const matched = numbers.filter(el => firstEl === fn(el));
+    const unMatched = numbers.filter(el => firstEl !== fn(el));
+    return groupBy(
+        unMatched,
+        fn,
+        {...bag, ...{[firstEl] : matched}}
+    )
+}
+
+groupBy([6.1, 6.2, 4.3, 4], Math.floor)
