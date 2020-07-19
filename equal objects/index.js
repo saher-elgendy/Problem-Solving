61792464c438993b275bf0285f5644a07da76db1
unknown <saherelgendy1990@gmail.com>
2020-07-19 05:27:02 -0700

    adding comments

--- equal objects/index.js
+++ equal objects/index.js
@@ -5,6 +5,8 @@
 //[{a: 1, b: 2}, {a: 1, b: 2, c: 3}, {a:2, b: 1 }] //false
 
 const checkObjectsKeysAndValuesEquality = (arr) => {
+    //looping through the array, check equality between the first object and the rest
+    //if any object doesn't have same values and keys of first object, return false
     for (let i = 1; i < arr.length; i++) {
         if (!areEqual(arr[0], arr[i])) return false;
     }
@@ -13,9 +15,10 @@ const checkObjectsKeysAndValuesEquality = (arr) => {
 }
 
 const areEqual = (a, b) => {
+    //extracting keys of two objects
     const aKeys = Object.keys(a);
     const bKeys = Object.keys(b);
-
+    //if any value related to the same key in both objects not equal, return false
     if (aKeys.length !== bKeys.length) return false;
 
     for (key in a) {
