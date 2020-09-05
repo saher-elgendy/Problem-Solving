0b41343829c63326bec37add9ae6e60df5a48e76
unknown <saherelgendy1990@gmail.com>
2020-09-05 05:42:06 -0700

    introducing solution 3

--- sum of multiples/sumOfMultiples.js
+++ sum of multiples/sumOfMultiples.js
@@ -25,20 +25,32 @@ multSum([1, 2, 3, 5, 9, 10]);//107
 //----------------------------------------------------------------------------
 
 //solution 2 
-
 const multSum = (arr) => {
   let newArr = arr.length % 2 === 1 ? [...arr, 1] : arr;
 
   let multiplesArr = [];
 
   for (let i = 0; i < newArr.length; i = i + 2) {
-    if (newArr[i + 1]) multiplesArr.push(newArr[i] * newArr[i + 1]);
+    if (newArr[i + 1] || newArr[i + 1] === 0) {
+      multiplesArr.push(newArr[i] * newArr[i + 1]);
+    }
   }
 
   return multiplesArr.reduce((acc, cur) => acc + cur, 0);
 }
 
 multSum([1, 2, 3, 6, 5, 8])//60
+
+//solution 3
+const multSum = arr => {
+  return arr.map((el, i) => {
+      if (i % 2 === 0) {
+          const nextEl = arr[i + 1];
+          return nextEl || nextEl === 0 ? el * nextEl : el * 1;
+      }
+      return 0;
+  }).reduce((acc, cur) => acc + cur, 0);
+}
 
 
 
