8002bf7fa80b2347189862f13876461b43809505
unknown <saherelgendy1990@gmail.com>
2020-09-05 05:20:48 -0700

    fixing solution 1 and refactor code

--- sum of multiples/sumOfMultiples.js
+++ sum of multiples/sumOfMultiples.js
@@ -12,11 +12,17 @@ multSum([1,2,5]) //  3 + 5 = 8
 
 
 //solution 1
+const multSum = arr => {
+  return arr.reduce((acc, cur, i, arr) => {
+    if (i % 2 === 0) {
+      return acc + (cur * (arr[i + 1] || arr[i + 1] === 0 ? arr[i + 1] : 1))
+    }
+    return acc;
+  }, 0)
+}
 
-const multSum = (arr) => arr.reduce((acc, cur, i) => i % 2 === 0 ? acc  + (cur * (arr[i + 1] ? arr[i + 1]  : 1)) : acc, 0);
-
-multSum([1,2,3,5,8,9,6]); //95 
-multSum([1,2,3,5,9,10]);//107
+multSum([1, 2, 3, 5, 8, 9, 6]); //95 
+multSum([1, 2, 3, 5, 9, 10]);//107
 
 //----------------------------------------------------------------------------
 
@@ -27,14 +33,14 @@ const multSum = (arr) => {
 
   let multiplesArr = [];
 
-  for(let i = 0; i < newArr.length; i = i + 2) {
-    if(newArr[i + 1]) multiplesArr.push(newArr[i] * newArr[i + 1]);
+  for (let i = 0; i < newArr.length; i = i + 2) {
+    if (newArr[i + 1]) multiplesArr.push(newArr[i] * newArr[i + 1]);
   }
-  
+
   return multiplesArr.reduce((acc, cur) => acc + cur, 0);
 }
 
-multSum([1,2,3,6,5,8])//60
+multSum([1, 2, 3, 6, 5, 8])//60
 
 
 
